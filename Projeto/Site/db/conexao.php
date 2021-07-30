c344612daa75621616cc16d6f7a53139da12b8c9
MateusOFCZ <mateusdacruz357@hotmail.com>
2021-07-30 19:04:49 -0300

    conexao.php Informações

--- Projeto/Site/db/conexao.php
+++ Projeto/Site/db/conexao.php
@@ -1,10 +1,10 @@
 <?php //Arquivo padrão de conexão do banco de dados.
     include_once 'conexao.php';
     
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASSWORD', '');
-    define('DB', 'sch');
+    define('HOST', 'IP DO SERVIDOR');
+    define('USER', 'USUÁRIO DO SERVIDOR');
+    define('PASSWORD', 'SENHA DO SERVIDOR');
+    define('DB', 'sch'); //Não é necessário alterar, apenas se modificado o nome do banco de dados.
     
     $connection = mysqli_connect(HOST, USER, PASSWORD, DB) or die ("Não foi possível conectar!")
 ?>
